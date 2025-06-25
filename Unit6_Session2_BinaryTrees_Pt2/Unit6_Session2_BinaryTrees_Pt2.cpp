77061303910f9f13ede1d674a7f396e7db6861f1
rrodrigoa <rrodrigoa@gmail.com>
2025-06-25 14:03:07 -0700

    Adding Unit 6 Session 2 code

---
+++ Unit6_Session2_BinaryTrees_Pt2/Unit6_Session2_BinaryTrees_Pt2.cpp
@@ -0,0 +1,102 @@
+﻿// Unit6_Session2_BinaryTrees_Pt2.cpp : Defines the entry point for the application.
+//
+
+#include <stack>
+#include "Unit6_Session2_BinaryTrees_Pt2.h"
+
+int indent_level = 0;
+
+// The node tree structure for the binary tree
+struct TreeNode {
+    int value;
+    TreeNode* leftNode;
+    TreeNode* rightNode;
+
+    TreeNode(int _value, TreeNode* _left, TreeNode* _right)
+        : value(_value), leftNode(_left), rightNode(_right) {}
+};
+
+// Adds operator << for struct TreeNode so its value can be printed to output
+std::ostream& operator<<(std::ostream& os, const TreeNode& node) {
+    os << node.value;
+    return os;
+}
+
+// Creates a Happy Case Tree as seen on slides
+//        1
+//      /   \
+//     2     3
+//    /   \
+//   4     5
+struct TreeNode* happy_case_tree() {
+    TreeNode* node4 = new TreeNode(4, nullptr, nullptr);
+    TreeNode* node5 = new TreeNode(5, nullptr, nullptr);
+    TreeNode* node3 = new TreeNode(3, nullptr, nullptr);
+    TreeNode* node2 = new TreeNode(2, node4, node5);
+    TreeNode* node1 = new TreeNode(1, node2, node3);
+
+    return node1;
+}
+
+// Creates an empty tree with nullptr as root
+//      nullptr
+TreeNode* empty_tree() {
+    return nullptr;
+}
+
+// Creates a single node tree
+//      1
+TreeNode* single_node_tree() {
+    TreeNode* node1 = new TreeNode(1, nullptr, nullptr);
+    return node1;
+}
+
+// Print PreOrder of a Tree using recursion
+void PrintPreOrder(TreeNode* root) {
+    INDENT_MORE();
+
+    if (root == nullptr) {
+        DEBUG_MSG("nullptr");
+        INDENT_LESS();
+
+        return;
+    }
+
+    std::cout << root->value << std::endl;
+    DEBUG_VAR(root->value);
+
+    PrintPreOrder(root->leftNode);
+    PrintPreOrder(root->rightNode);
+
+    INDENT_LESS();
+}
+
+// Print PreOrder of a Tree using a stack
+void PrintPreOrderStack(TreeNode* root) {
+    std::stack<TreeNode*> stack;
+    stack.push(root);
+
+    while (stack.empty() == false) {
+        TreeNode* top = stack.top();
+        stack.pop();
+
+        if (top == nullptr) {
+            continue;
+        }
+
+        std::cout << top->value << " " << std::endl;
+        stack.push(top->rightNode);
+        stack.push(top->leftNode);
+    }
+}
+
+int main()
+{
+    TreeNode* root = happy_case_tree();
+
+    PrintPreOrder(root);
+
+    PrintPreOrderStack(root);
+
+    return 0;
+}
---
+++ Unit6_Session2_BinaryTrees_Pt2/Unit6_Session2_BinaryTrees_Pt2.h
@@ -0,0 +1,45 @@
+﻿#pragma once
+
+#include <iostream>
+#include <stack>
+
+extern int indent_level;
+
+// Simple console print functions for debugging
+#define INDENT std::string(indent_level * 2, ' ')
+#define INDENT_MORE() indent_level++
+#define INDENT_LESS() indent_level--
+#define DEBUG_VAR(var) std::cout << INDENT << "[DEBUG] " << __func__ << " (line " << __LINE__ << "): " << #var << " = " << (var) << std::endl;
+#define DEBUG_MSG(msg) std::cout << INDENT << "[DEBUG] " << __func__ << " (line " << __LINE__ << "): " << msg << std::endl;
+//int indent_level = 0;
+
+// Template << operator for stack<T>
+// Print stack as [elements..]
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const std::stack<T> s) {
+    std::stack<T> s_copy = s;
+    os << "[";
+    while (s_copy.empty() == false) {
+        if constexpr (std::is_pointer_v<T>) {
+            if (s_copy.top() != nullptr)
+                os << *(s_copy.top());
+            else
+                os << "nullptr";
+        }
+        else {
+            if (s_copy.top() != nullptr)
+                os << s_copy.top();
+            else
+                os << "nullptr";
+        }
+
+        s_copy.pop();
+
+        if (s_copy.empty() == false)
+        {
+            os << ", ";
+        }
+    }
+    os << "]";
+    return os;
+}
